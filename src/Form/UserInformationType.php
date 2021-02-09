a7cadbe41523c4606b9e37a68842262f554d210c
François <francois.leclere2020@campus-eni.fr>
2021-02-09 17:05:46 +0100

    fix(Profil) : fix du changement de mot de passe

--- src/Form/UserInformationType.php
+++ src/Form/UserInformationType.php
@@ -26,16 +26,16 @@ class UserInformationType extends AbstractType
             ->add('nom')
             ->add('telephone')
             ->add('mail', EmailType::class)
-            ->add('mot_de_passe', RepeatedType::class,
+            ->add('password', RepeatedType::class,
             [
                 'mapped' => false,
                 'type' => PasswordType::class,
                 'invalid_message' => 'The password fields must match.',
-                'first_options'  => ['label' => 'Password'],
+                'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Repeat Password'],
                 'constraints' => [
                     new Length([
-                        'min' => 6,
+                        'min' => 4,
                         'minMessage' => 'Your password should be at least {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
