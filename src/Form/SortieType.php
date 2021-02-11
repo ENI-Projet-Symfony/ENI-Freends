64632aea1c24a5c575594f04551e82e4704c47df
Damien PUAUD <damien.puaud22020@campus-eni.fr>
2021-02-11 15:27:50 +0100

    <feature> (Sortie) : Upload d'image dans les sorties

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -10,24 +10,41 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('picture', FileType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'class' => "custom-file-input"
+                ],
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '8000k',
+                        'maxSizeMessage' => 'Image trop lourde. 2Mb maximum',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                    ])
+                ]
+            ])
             ->add('nom',TextType::class,[
                 'label' => "Nom de la Sortie",
                 'attr' => [
                     'class' => "form-control"
                 ]
-
             ])
             ->add('dateHeureDebut',DateTimeType::class,[
                 'label' => "Date et heure de la sortie",
