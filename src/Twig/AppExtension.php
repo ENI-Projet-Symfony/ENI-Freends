23d26fb01d6c23a5291afc0b6e8d0a628f2f0379
Damien PUAUD <damien.puaud22020@campus-eni.fr>
2021-02-12 10:33:10 +0100

    feature (Global) : Création extension Twig pour detection mobile

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,56 @@
+<?php
+// src/Twig/AppExtension.php
+namespace App\Twig;
+
+use DeviceDetector\DeviceDetector;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+
+    private $kernelProjectDir;
+
+    // le constructeur indique que nous avons ici besoin d'un paramètre, le kernelProjectDir.
+    // Nous allons passer ce paramètre en le déclarant dans notre fichier config/services.yaml.
+    // Il s'agit d'un paramètre par défaut introduit dans Symfony 4.
+    public function __construct(string $kernelProjectDir)
+    {
+        $this->kernelProjectDir = $kernelProjectDir;
+    }
+
+
+    public function getFunctions()
+    {
+        return array(
+            // on déclare notre fonction.
+            // Le 1er paramètre est le nom de la fonction utilisée dans notre template
+            // le 2ème est un tableau dont le 1er élément représente la classe où trouver la fonction associée (en l'occurence $this, c'est à dire cette classe puisque notre fonction est déclarée un peu plus bas). Et le 2ème élément du tableau est le nom de la fonction associée qui sera appelée lorsque nous l'utiliserons dans notre template.
+            new TwigFunction('isMobile', array($this, 'isMobileFunction')),
+            new TwigFunction('isDesktop', array($this, 'isDesktopFunction')),
+        );
+    }
+
+    // chemin relatif de notre fichier en paramètre
+    public function isMobileFunction()
+    {
+        // si le fichier passé en paramètre de la fonction existe, on retourne true,
+        // sinon on retourne false.
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $dd = new DeviceDetector($userAgent);
+        $dd->parse();
+
+        return $dd->isMobile() ? true : false;
+    }
+
+    public function isDesktopFunction()
+    {
+        // si le fichier passé en paramètre de la fonction existe, on retourne true,
+        // sinon on retourne false.
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $dd = new DeviceDetector($userAgent);
+        $dd->parse();
+
+        return $dd->isDesktop() ? true : false;
+    }
+}
